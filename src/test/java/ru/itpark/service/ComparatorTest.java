c244f892f4b70381601cda7ade188c6536daa795
Damir <alekbaevd@yandex.ru>
2019-01-14 20:37:06 +0300

    Исправил ComparatorTest, добавил stream

--- src/test/java/ru/itpark/service/ComparatorTest.java
+++ src/test/java/ru/itpark/service/ComparatorTest.java
@@ -9,7 +9,9 @@ import ru.itpark.domain.House;
 import ru.itpark.repository.HouseRepository;
 
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,39 +32,32 @@ class ComparatorTest {
     @Test
     void priceDecComparatorTest() {
         List<House> list = service.getSorted(new PriceDecComparator());
-        int[] result = new int[list.size()];
-        int i = 0;
-        for (House house : service.getAll()) {
-            result[i] = house.getPrice();
-            i++;
-        }
-        int[] expected = {14_000_000, 10_000_000, 4_000_000, 1_500_000};
-        assertArrayEquals(expected, result);
+        List<Integer> result = list.stream()
+                .map(House::getPrice)
+                .collect(Collectors.toList());
+
+        List<Integer> expected = Arrays.asList(14_000_000, 10_000_000, 4_000_000, 1_500_000);
+        assertEquals(expected, result);
     }
 
     @Test
     void priceIncComparatorTest() {
         List<House> list = service.getSorted(new PriceIncComparator());
-        int[] result = new int[list.size()];
-        int i = 0;
-        for (House house : service.getAll()) {
-            result[i] = house.getPrice();
-            i++;
-        }
-        int[] expected = {1_500_000, 4_000_000, 10_000_000, 14_000_000};
-        assertArrayEquals(expected, result);
+        List<Integer> result = list.stream()
+                .map(House::getPrice)
+                .collect(Collectors.toList());
+        List<Integer> expected = Arrays.asList(1_500_000, 4_000_000, 10_000_000, 14_000_000);
+        assertEquals(expected, result);
     }
 
     @Test
     void dateDecComparatorTest() {
         List<House> list = service.getSorted(new DateDecComparator());
-        String[] result = new String[list.size()];
-        int i = 0;
-        for (House house : service.getAll()) {
-            result[i] = house.getDate();
-            i++;
-        }
-        String[] expected = {"17.11.2018", "17.10.2018", "15.10.2018", "17.09.2018"};
-        assertArrayEquals(expected, result);
+        List<String> result = list.stream()
+                .map(House::getDate)
+                .collect(Collectors.toList());
+
+        List<String> expected = Arrays.asList("17.11.2018", "17.10.2018", "15.10.2018", "17.09.2018");
+        assertEquals(expected, result);
     }
 }
